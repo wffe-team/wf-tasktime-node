5afde4a68ea82d835f0a2ff96b27495a69b1003f
tianyongxin <tianyongxin@TIANYONGXIN>
2017-11-29 17:09:23 +0800

    修改

--- routes/department.js
+++ routes/department.js
@@ -4,7 +4,7 @@ import express from 'express'
 import Department from '../controller/department/department'
 const router = express.Router()
 
-router.post('createDepartment', Department.createDepartment);
+router.post('/createDepartment', Department.createDepartment);
 router.get('/getDepartment', Department.getDepartment);
 router.get('/getProjectFirst', Department.getProjectFirst);
 router.get('/getProjectSecond', Department.getProjectSecond);
